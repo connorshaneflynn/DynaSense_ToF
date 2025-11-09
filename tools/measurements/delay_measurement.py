02ca571a7ca8fb79ed82153decd56a04de0b9e5c
cflynn <connor.flynn@hispeed.ch>
2025-11-09 16:07:58 +0100

    tools: delay_measurement works with CDC and HID

--- tools/measurements/delay_measurement.py
+++ tools/measurements/delay_measurement.py
@@ -1,13 +1,16 @@
 import sys
 import time
 import serial
+import hid
 import struct
 import numpy as np
 from PySide6 import QtWidgets, QtCore
 import pyqtgraph as pg
 from collections import defaultdict, deque
 
-SERIAL_PORT = 'COM4'  # COM4 for USB, COM6 for UART
+COM = 'CDC'  # HID or CDC
+
+SERIAL_PORT = '/dev/ttyACM1'  # COM4 for USB, COM6 for UART, or /dev/ttyACM* on ubuntu
 BAUD_RATE = 230400
 RESOLUTION = 16
 FRAME_SIZE = 2 + 1 + RESOLUTION * 2 + RESOLUTION  # header + ID + distances + statuses
@@ -18,8 +21,39 @@ UPDATE_HZ = 120  # output framerate (Hz)
 
 # start_time = 0
 
+def read_frame_bytes(packet):
+        """Reads the raw bytes from serial and packs them into the frame data.
+        Returns sensor_ID, distances, and statuses."""
+        # Read sensor ID
+        sensor_ID = packet[0]
 
-def read_frame(ser):
+        # Read distances
+        # raw = input.read(RESOLUTION * 2)
+        raw = packet[1:RESOLUTION*2+1]
+        if not raw:
+            return None, None, None
+        distances = np.array(struct.unpack("<" + "H" * RESOLUTION, raw))
+
+        # Read statuses (1 byte each)
+        # raw = input.read(RESOLUTION)
+        raw = packet[RESOLUTION*2+1:]
+        if not raw:
+            return None, None, None
+        statuses = np.array(struct.unpack("<" + "B" * RESOLUTION, raw))
+
+        # # when bitpacking is used, each status is 1 bit
+        # num_status_bytes = RESOLUTION // 8
+        # status_bytes = self.ser.read(num_status_bytes)
+        # statuses = []
+        # for i in range(RESOLUTION):
+        #     byte_idx = i // 8
+        #     bit_idx = i % 8
+        #     statuses.append((status_bytes[byte_idx] >> bit_idx) & 1)
+        # statuses = np.array(statuses)
+
+        return sensor_ID, distances, statuses
+
+def CDC_read_frame(ser):
     # Sync to header, repeat periodically if failed connection
     while True:
         try:
@@ -30,31 +64,20 @@ def read_frame(ser):
             time.sleep(1)
             # try to reconnect
             try:
-                ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=0.001)
+                ser = serial.Serial(SERIAL_PORT, BAUD_RATE)
             except serial.SerialException:
                 pass
-    # global start_time
-    # start_time = time.time_ns()
+    packet = ser.read(FRAME_SIZE - 2)
+    return read_frame_bytes(packet)
 
-    # Read sensor ID
-    raw = ser.read(1)
-    if not raw:
-        return None, None, None
-    sensor_ID = int.from_bytes(raw, byteorder='big')
 
-    # Read distances
-    raw = ser.read(RESOLUTION * 2)
-    if not raw:
-        return None, None, None
-    distances = np.array(struct.unpack("<" + "H" * RESOLUTION, raw))
+def HID_read_frame(dev_handle):
+    # No syncing as complete packet per frame
 
-    # Read statuses (1 byte each)
-    raw = ser.read(RESOLUTION)
-    if not raw:
-        return None, None, None
-    statuses = np.array(struct.unpack("<" + "B" * RESOLUTION, raw))
+    packet = dev_handle.read(FRAME_SIZE)
+    packet = packet[2:]
+    return read_frame_bytes(packet)
 
-    return sensor_ID, distances, statuses
 
 
 def filter_data(data, statuses):
@@ -68,25 +91,43 @@ def filter_data(data, statuses):
 
 
 def output_data(data):
-     print(data, flush=True)
+     print(data, flush=True, end="    ")
 
 
 if __name__ == "__main__":
-    try:
-        ser = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=0.001)
-    except serial.SerialException:
-            print(f"\nERROR: Could not open port '{SERIAL_PORT}'. Check if device is connected and running.\n")
-            raise SystemExit()
+    # initialize COM
+    if COM == 'CDC':
+        try:
+            dev = serial.Serial(SERIAL_PORT, BAUD_RATE, timeout=0.001)
+        except serial.SerialException:
+                print(f"\nERROR: Could not open port '{SERIAL_PORT}'. Check if device is connected and running.\n")
+                raise SystemExit()
+    elif COM == 'HID':
+        try:
+            dev = hid.Device(0x0483, 0x5750)
+        except hid.HIDException:
+            print(f"Could not open HID device.")
+            sys.exit()
+    else:
+        print("Error: Unknown COM")
+        sys.exit()
     
     while True:
         try:
-            _, data, statuses = read_frame(ser)
+            t_start = time.perf_counter_ns()
+            if COM == 'CDC':
+                _, data, statuses = CDC_read_frame(dev)
+            else:
+                _, data, statuses = HID_read_frame(dev)
+            t_delta = round((time.perf_counter_ns() - t_start) / 1e6, 1)
+            print(t_delta)
 
             data, statuses = filter_data(data, statuses)
             output_data(data)
             # delta_time = time.time_ns() - start_time
             # print(f"\t{delta_time/(1e6):03f}")
+            
 
         except KeyboardInterrupt:
-            ser.close()
+            dev.close()
             sys.exit()
